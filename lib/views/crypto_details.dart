331edc2d86fdf0d30b3554a5161e1f96de51a8d2
Nina2498 <shene1998m@gmail.com>
2024-03-14 12:55:14 +0300

    UI update

--- lib/views/crypto_details.dart
+++ lib/views/crypto_details.dart
@@ -1,27 +1,20 @@
-// import 'package:crypto_price_tracker/models/crypto_model.dart';
-// import 'package:flutter/material.dart';
+import 'package:crypto_price_tracker/models/crypto_model.dart';
+import 'package:flutter/material.dart';
 
-// class CoinDetailPage extends StatelessWidget {
-//   final TickerData tickerData;
+class CryptoDetailsPage extends StatelessWidget {
+  final String cryptoName;
 
-//   CoinDetailPage({required this.tickerData});
+  CryptoDetailsPage({required this.cryptoName});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(tickerData.name),
-//       ),
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         crossAxisAlignment: CrossAxisAlignment.center,
-//         children: [
-//           Text('Price: ${tickerData.price}'),
-//           Text('High: ${tickerData.high}'),
-//           Text('Low: ${tickerData.low}'),
-//           Text('Change: ${tickerData.change}'),
-//         ],
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Crypto Details'),
+      ),
+      body: Center(
+        child: Text('Details for $cryptoName'),
+      ),
+    );
+  }
+}
--- lib/views/crypto_view.dart
+++ lib/views/crypto_view.dart
@@ -1,60 +1,90 @@
 import 'package:crypto_price_tracker/blocs/app_events.dart';
 import 'package:crypto_price_tracker/models/crypto_model.dart';
+import 'package:crypto_price_tracker/views/crypto_details.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:crypto_price_tracker/blocs/crypto_bloc.dart';
 import 'package:crypto_price_tracker/blocs/app_states.dart';
 import 'package:crypto_price_tracker/services/crypto_service.dart';
 
+@override
 class CryptoView extends StatelessWidget {
-  const CryptoView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color:
+            Color.fromARGB(255, 15, 71, 64), // Dark shade of background color
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 340, // Adjust height as needed
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/imgs/btn.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+                height: 20), // Add some space between the image and the list
+            Expanded(
+              child: CryptoListView(), // List of the top 10 currencies
+            ),
+            CryptoSlider(), // Slider of each currency at the bottom
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CryptoListView extends StatelessWidget {
+  final List<String> cryptoNames = [
+    'Bitcoin',
+    'Ethereum',
+    'Litecoin',
+    'Ripple',
+    'Bitcoin Cash',
+    'Cardano',
+    'Stellar',
+    'Chainlink',
+    'Polkadot',
+    'Binance Coin',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-        create: (context) => AppBloc(
-              RepositoryProvider.of<CryptoRepo>(context),
-            )..add(AppLoad()),
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Crypto Market Prices'),
-          ),
-          body: BlocBuilder<AppBloc, AppState>(
-            builder: (context, state) {
-              if (state is AppLoadingState) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-              if (state is ExchangeErrorState) {
-                return Center(
-                  child: Text('Error loading data: ${state.error}'),
-                );
-              }
-              if (state is AppLoadedState) {
-                List<CryptoModel> data = state.data;
-                return ListView.builder(
-                  itemCount: data.length,
-                  itemBuilder: (context, index) {
-                    CryptoModel crypto = data[index];
-                    return ListTile(
-                      title: Text(crypto.name),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Price: ${crypto.price.toString()}'),
-                          Text('High: ${crypto.high.toString()}'),
-                          Text('Low: ${crypto.low.toString()}'),
-                          Text('Change: ${crypto.change.toString()}'),
-                        ],
-                      ),
-                    );
-                  },
-                );
-              }
-              return Container();
-            },
-          ),
-        ));
+    return ListView.builder(
+      itemCount: cryptoNames.length,
+      itemBuilder: (context, index) {
+        final cryptoName = cryptoNames[index];
+        return ListTile(
+          title: Text(cryptoName),
+          onTap: () {
+            // Perform action when tapped, for example, navigate to details page
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      CryptoDetailsPage(cryptoName: cryptoName)),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class CryptoSlider extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // You can implement a slider here with details of each currency
+    return Container(
+        // Slider implementation
+        );
   }
 }
